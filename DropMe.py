f36dae35ae5bc1a67b2890aeda0dc9258b24d746
geneus003 <geneus003@gmail.com>
2018-01-07 01:22:34 +0300

    Some changes

--- DropMe.py
+++ DropMe.py
@@ -1,13 +1,24 @@
 from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QLineEdit, QPushButton)
-from PyQt5.QtGui import (QPixmap, QFont, QMouseEvent)
-from PyQt5.QtCore import (Qt, QEvent, pyqtSignal, QCoreApplication, QObject)
+from PyQt5.QtGui import (QPixmap, QFont, QIcon)
+from PyQt5.QtCore import (Qt, QSize)
 import sys
 
 app = QApplication(sys.argv)
 root = QWidget()
+wind = 1
 
 
-class RegisterScreenVar():
+def check_log(event):
+    log_screen_var.__anext__()
+    drop_me(2)
+
+
+def new_acc_fun(event):
+    log_screen_var.__anext__()
+    drop_me(3)
+
+
+class LoginScreenVar():
 
     def __init__(self):
 
@@ -15,26 +26,52 @@ class RegisterScreenVar():
         self.drop_png = QPixmap("images/DropMeLogo.png")
         self.login_but = QLabel(root)
         self.login_but_img = QPixmap("images/Login_but.png")
+        self.account_hint_img = QPixmap("images/account-hint.png")
         self.login_lab = QLabel('Login', root)
         self.pas_lab = QLabel('Password', root)
         self.login_box = QLineEdit(root)
         self.reg_box = QLineEdit(root)
-        self.new_account = QPushButton("Don't have an account?", root)
+        self.new_account = QLabel("Don't have an account?", root)
+
         self.initui()
 
     def initui(self):
 
-        self.new_account.clicked.connect(self.printt)
+        self.new_account.mousePressEvent = new_acc_fun
+        self.login_but.mousePressEvent = check_log
 
-    def printt(self):
-        print("hello")
+    def __anext__(self):
 
+        self.new_account.deleteLater()
+        self.drop_logo.deleteLater()
+        self.login_box.deleteLater()
+        self.login_but.deleteLater()
+        self.login_lab.deleteLater()
+        self.pas_lab.deleteLater()
+        self.reg_box.deleteLater()
+
+
+class RegisterScreenVar():
+
+    def __init__(self):
+
+        self.register_lab = QLabel("Register")
+
+
+log_screen_var = LoginScreenVar()
 reg_screen_var = RegisterScreenVar()
 
 
-def drop_me():
+def drop_me(wind):
 
-    def register_screen(reg_screen_var, root, app):
+    def check_acc_ser():
+        print("loading")
+
+    def register_screen(reg_screen_war, root):
+
+        reg_screen_var.register_lab.move(100, 100)
+
+    def login_screen(log_screen_var, root):
         # design of register screen
 
         root.resize(1280, 720)
@@ -42,39 +79,48 @@ def drop_me():
         root.setWindowTitle('Drop_Me')
         root.setStyleSheet('background-image: url(images/reg_back.png);')
 
-        reg_screen_var.login_but.setPixmap(reg_screen_var.login_but_img)
-        reg_screen_var.login_but.setAttribute(Qt.WA_TranslucentBackground, True)
-        reg_screen_var.login_but.move(520, 500)
+        log_screen_var.login_but.setPixmap(log_screen_var.login_but_img)
+        log_screen_var.login_but.setAttribute(Qt.WA_TranslucentBackground, True)
+        log_screen_var.login_but.move(520, 500)
 
-        reg_screen_var.drop_logo.setPixmap(reg_screen_var.drop_png)
-        reg_screen_var.drop_logo.setAttribute(Qt.WA_TranslucentBackground, True)
-        reg_screen_var.drop_logo.move(370, 0)
+        log_screen_var.drop_logo.setPixmap(log_screen_var.drop_png)
+        log_screen_var.drop_logo.setAttribute(Qt.WA_TranslucentBackground, True)
+        log_screen_var.drop_logo.move(370, 0)
 
-        reg_screen_var.login_lab.setFont(QFont("OpenSans-Regular", 20, QFont.Bold))
-        reg_screen_var.login_lab.setStyleSheet('QLabel {color: #F6F6F6}')
-        reg_screen_var.login_lab.setAttribute(Qt.WA_TranslucentBackground, True)
-        reg_screen_var.login_lab.move(580, 230)
+        log_screen_var.login_lab.setFont(QFont("OpenSans-Regular", 20, QFont.Bold))
+        log_screen_var.login_lab.setStyleSheet('QLabel {color: #F6F6F6}')
+        log_screen_var.login_lab.setAttribute(Qt.WA_TranslucentBackground, True)
+        log_screen_var.login_lab.move(580, 230)
 
-        reg_screen_var.pas_lab.setFont(QFont("OpenSans-Regular", 20, QFont.Bold))
-        reg_screen_var.pas_lab.setStyleSheet('QLabel {color: #F6F6F6}')
-        reg_screen_var.pas_lab.setAttribute(Qt.WA_TranslucentBackground, True)
-        reg_screen_var.pas_lab.move(550, 350)
+        log_screen_var.pas_lab.setFont(QFont("OpenSans-Regular", 20, QFont.Bold))
+        log_screen_var.pas_lab.setStyleSheet('QLabel {color: #F6F6F6}')
+        log_screen_var.pas_lab.setAttribute(Qt.WA_TranslucentBackground, True)
+        log_screen_var.pas_lab.move(550, 350)
 
-        reg_screen_var.login_box.setFont(QFont("OpenSans-Regular", 18, QFont.Bold))
-        reg_screen_var.login_box.setStyleSheet('QLineEdit {color:black; width: 300px}')
-        reg_screen_var.login_box.move(470, 290)
+        log_screen_var.login_box.setFont(QFont("OpenSans-Regular", 18, QFont.Bold))
+        log_screen_var.login_box.setStyleSheet('QLineEdit {color:black; width: 300px}')
+        log_screen_var.login_box.move(470, 290)
 
-        reg_screen_var.reg_box.setFont(QFont("OpenSans-Regular", 18, QFont.Bold))
-        reg_screen_var.reg_box.setStyleSheet('QLineEdit {color:black; width: 300px}')
-        reg_screen_var.reg_box.move(470, 400)
+        log_screen_var.reg_box.setFont(QFont("OpenSans-Regular", 18, QFont.Bold))
+        log_screen_var.reg_box.setStyleSheet('QLineEdit {color:black; width: 300px}')
+        log_screen_var.reg_box.move(470, 400)
 
-        reg_screen_var.new_account.setFont(QFont("OpenSans-Regular", 10, QFont.Bold))
-        reg_screen_var.new_account.move(420, 580)
+        log_screen_var.new_account.move(540, 600)
+        log_screen_var.new_account.setAttribute(Qt.WA_TranslucentBackground, True)
+        log_screen_var.new_account.setPixmap(log_screen_var.account_hint_img)
 
-        root.show()
+    if wind == 1:
+        login_screen(log_screen_var, root)
 
-        sys.exit(app.exec_())
+    elif wind == 2:
+        register_screen(reg_screen_var, root)
 
-    register_screen(reg_screen_var, root, app)
+    elif wind == 3:
+        check_acc_ser()
 
-drop_me()
+    root.show()
+
+
+drop_me(wind)
+
+sys.exit(app.exec_())
